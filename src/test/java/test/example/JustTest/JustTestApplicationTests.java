35a0728ed2d9c200ae68fedaa0d9d16cd9863535
user <dkzks456@gmail.com>
2024-08-23 13:10:51 +0900

    clear baaaaaaaam

---
+++ src/test/java/test/example/JustTest/JustTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.example.JustTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
